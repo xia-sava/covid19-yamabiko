137a12d1bd9063052b7a2e8d766ffe622e6ffa91
ISIGE, Akira <xia@silvia.com>
2021-08-27 20:04:39 +0900

    Sentry 対応

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ class Main
         $this->client->setServerParameters([
             'HTTP_USER_AGENT' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
         ]);
+        Sentry\init(['dsn' => $_ENV['SENTRY_DSN'] ?? '' ]);
     }
 
     public function main(): void
